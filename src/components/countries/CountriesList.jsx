e6e06d187d00f8ec3f406c981e0e337d85670031
Denis Leonenko <leograndprix@gmail.com>
2025-03-26 21:25:47 +0300

    alpha

--- src/components/countries/CountriesList.jsx
+++ src/components/countries/CountriesList.jsx
@@ -1,13 +1,8 @@
-import { useSelector } from 'react-redux'
-
 import { CountriesItem } from './CountriesItem'
-import { selectCountries } from '../../redux/slices/countriesSlice'
 
 import styles from './Countries.module.css'
 
 export const CountriesList = ({ filteredCountries }) => {
-  const countries = useSelector(selectCountries)
-
   return (
     <>
       <section className={styles.countriesItems}>
