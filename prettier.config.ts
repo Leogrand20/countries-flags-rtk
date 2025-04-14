a41e300d2c8f44adb2795b27c7efd5c8ad57262a
Denis Leonenko <leograndprix@gmail.com>
2025-04-14 22:43:06 +0300

    Typed prettier.config

---
+++ prettier.config.ts
@@ -0,0 +1,15 @@
+interface Config {
+  tabWidth: number
+  printWidth: number
+  semi: boolean
+  singleQuote: boolean
+}
+
+const config: Config = {
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+}
+
+export default config
