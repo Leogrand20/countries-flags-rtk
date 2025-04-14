e37f6d70051f7c6a774362f456608a4d945a31bd
Denis Leonenko <leograndprix@gmail.com>
2025-04-14 21:53:28 +0300

    Typed CountriesList

--- src/components/countries/CountriesList.tsx
+++ src/components/countries/CountriesList.tsx
@@ -1,11 +1,14 @@
+import { FC } from 'react'
+
 import { CountriesItem } from './CountriesItem'
+import { CountriesListProps } from '../../types/countries'
 
 import styles from './Countries.module.css'
 
-export const CountriesList = ({ countries }) => {
+export const CountriesList: FC<CountriesListProps> = ({ countries }) => {
   return (
     <>
-      <section className={styles.countriesItems}>
+      <section className={styles['countriesItems']}>
         {countries.map((country) => (
           <CountriesItem key={country.id} {...country} />
         ))}
