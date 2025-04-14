490093533c71dfd289ea8f482791f85579daaa4c
Denis Leonenko <leograndprix@gmail.com>
2025-04-14 22:32:33 +0300

    Typed useSearch

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,14 +1,14 @@
+import { ChangeEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { setSearchFilter } from '../redux/slices/filterSlice'
-
 import { selectSearchFilter } from '../redux/selectors/filter-selectors'
 
 export const useSearch = () => {
   const dispatch = useDispatch()
   const search = useSelector(selectSearchFilter)
 
-  const handleSetSearchFilter = (e) => {
+  const handleSetSearchFilter = (e: ChangeEvent<HTMLInputElement>): void => {
     dispatch(setSearchFilter(e.target.value))
   }
 
